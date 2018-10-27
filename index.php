83b5d27adc061c5c6be6c430d35c1abf3ada281e
Pao Daza <gipaodaza@gmail.com>
2018-10-27 11:19:09 -0500

    commit

--- index.php
+++ index.php
@@ -36,7 +36,6 @@ Al anochecer, cuando los enormes carros iban camino de las eras, cargados de gav
 
 <img src="descarga.jpg" alt="">
 <a href="https://www.youtube.com/watch?v=UbqMTtVZ9EU">CUENTO CORTO</a>
-hola
-
+hol
 </body>
 </html>
